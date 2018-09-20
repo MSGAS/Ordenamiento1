032108a1a1f1976554dae7c43fcfc5696f1dc67c
Misael <msgas2_@outlook.com>
2018-09-20 09:53:11 -0500

    Intercambio inicial de proyecto

---
+++ Demo.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Demo
+{
+    static void ordenarEnteros(int maxEnteros){
+        int enteros[];
+        enteros = new int[maxEnteros];
+        
+        for(int i=0; i < enteros.length; i++){
+            enteros[i]=(int)(Math.random() * maxEnteros);
+        }
+        
+        Arrays.sort(enteros);
+        
+        for(int i=0; i<enteros.length; i++){
+            System.out.println(enteros[i]);
+        }
+    }
+
+}
